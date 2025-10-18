2983de7d0eeafc8214999984010237d5762189f2
Saul Barreto <ti.soe.info@gmail.com>
2025-10-17 21:46:57 -0500

    limpiando el 2do ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RSBM';
-  subTitle : string = ' es la persona que esta Logueada!'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PadreComponent } from './padre/padre.component';
-import { HijoComponent } from './hijo/hijo.component';
 import { FormsModule } from '@angular/forms';
-import { HermanoComponent } from './hermano/hermano.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PadreComponent,
-    HijoComponent,
-    HermanoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
